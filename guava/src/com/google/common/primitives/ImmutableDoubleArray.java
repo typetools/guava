695cc015e78a4b2dd866a5e39dc0fdcda483607a
Suzanne Millstein <smillst@cs.washington.edu>
2018-01-26 12:51:07 -0800

    Remove unneed annotations
    While these @SameLen annotations are true, (or would be if they were in the correct location), they aren't needed.

--- guava/src/com/google/common/primitives/ImmutableDoubleArray.java
+++ guava/src/com/google/common/primitives/ImmutableDoubleArray.java
@@ -512,15 +512,7 @@ public final class ImmutableDoubleArray implements Serializable {
   }
 
   /** Returns a new, mutable copy of this array's values, as a primitive {@code double[]}. */
-  @SuppressWarnings({
-    /*
-     * length of this is defined as end-start,
-     * Arrays.copyOfRange returns an array of length end-start,
-     * ẗherefore the result is SameLen("this")
-     */
-    "samelen:return.type.incompatible", // SameLen for custom coll. with size end-start
-  }) 
-  public @SameLen("this") double[] toArray() {
+  public double [] toArray() {
     return Arrays.copyOfRange(array, start, end);
   }
 
