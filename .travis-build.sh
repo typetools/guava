d24adfa23b2f34cfece28740feeaaad75b67d9bb
Michael Ernst <mernst@cs.washington.edu>
2019-06-21 16:46:53 -0700

    Path munging

--- .travis-build.sh
+++ .travis-build.sh
@@ -31,10 +31,9 @@ echo ROOT=$ROOT
 [ -d /tmp/plume-scripts ] || (cd /tmp && git clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git)
 REPO=`/tmp/plume-scripts/git-find-fork ${SLUGOWNER} typetools checker-framework`
 BRANCH=`/tmp/plume-scripts/git-find-branch ${REPO} ${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}`
-(cd $ROOT && git clone -b ${BRANCH} --single-branch --depth 1 -q ${REPO} checker-framework) || (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 -q ${REPO} checker-framework)
+(cd .. && git clone -b ${BRANCH} --single-branch --depth 1 -q ${REPO} checker-framework) || (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 -q ${REPO} checker-framework)
 # This also builds annotation-tools and jsr308-langtools
-(cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh downloadjdk)
-export CHECKERFRAMEWORK=$ROOT/checker-framework
+(cd ../checker-framework/ && ./.travis-build-without-test.sh downloadjdk)
+export CHECKERFRAMEWORK=`readlink ../checker-framework`
 
-cd guava
-mvn package -P checkerframework-local -Dmaven.test.skip=true -Danimal.sniffer.skip=true
+(cd guava && mvn package -P checkerframework-local -Dmaven.test.skip=true -Danimal.sniffer.skip=true)
