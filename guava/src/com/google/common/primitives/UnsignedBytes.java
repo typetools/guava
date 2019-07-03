7a25627c3880f99fb8d0cb87ee2772984e5d487d
Kartikeya Goswami <kartikeyagoswami20@gmail.com>
2019-07-04 01:06:51 +0530

    Update UnsignedBytes.java

--- guava/src/com/google/common/primitives/UnsignedBytes.java
+++ guava/src/com/google/common/primitives/UnsignedBytes.java
@@ -30,10 +30,10 @@ import org.checkerframework.checker.index.qual.NonNegative;
 import org.checkerframework.checker.index.qual.Positive;
 import org.checkerframework.common.value.qual.IntRange;
 import org.checkerframework.common.value.qual.MinLen;
+import org.checkerframework.checker.signedness.qual.PolySigned;
 import org.checkerframework.checker.signedness.qual.Unsigned;
 import org.checkerframework.checker.signedness.qual.UnknownSignedness;
 import org.checkerframework.checker.signedness.qual.SignednessGlb;
-import org.checkerframework.checker.signedness.qual.PolySigned;
 import sun.misc.Unsafe;
 
 /**
