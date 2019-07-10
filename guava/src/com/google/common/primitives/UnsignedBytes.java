f72c650d46e039b047db5ed100506338699acaed
Kartikeya Goswami <kartikeyagoswami20@gmail.com>
2019-07-10 13:57:10 +0530

    Update UnsignedBytes.java

--- guava/src/com/google/common/primitives/UnsignedBytes.java
+++ guava/src/com/google/common/primitives/UnsignedBytes.java
@@ -33,7 +33,7 @@ import org.checkerframework.common.value.qual.MinLen;
 import org.checkerframework.checker.signedness.qual.PolySigned;
 import org.checkerframework.checker.signedness.qual.Unsigned;
 import org.checkerframework.checker.signedness.qual.UnknownSignedness;
-import org.checkerframework.checker.signedness.qual.SignednessGlb;
+import org.checkerframework.checker.signedness.qual.SignedPositive;
 import sun.misc.Unsafe;
 
 /**
@@ -81,7 +81,7 @@ public final class UnsignedBytes {
    *
    * @since 6.0
    */
-  public static @NonNegative @SignednessGlb int toInt(@UnknownSignedness byte value) {
+  public static @NonNegative @SignedPositive int toInt(@UnknownSignedness byte value) {
     return value & UNSIGNED_MASK;
   }
 
@@ -107,7 +107,7 @@ public final class UnsignedBytes {
    * @return {@code (byte) 255} if {@code value >= 255}, {@code (byte) 0} if {@code value <= 0}, and
    *     {@code value} cast to {@code byte} otherwise
    */
-  public static @Unsigned byte saturatedCast(@UnknownSignedness long value) {
+  public static @Unsigned byte saturatedCast(long value) {
     if (value > toInt(MAX_VALUE)) {
       return MAX_VALUE; // -1
     }
